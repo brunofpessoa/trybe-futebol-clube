7012452d6948fd2360645198899f83dae308603d
Bruno Pessoa <brunofernpessoa@gmail.com>
2022-11-27 16:10:07 -0300

    Cria migration e model de matches

---
+++ app/backend/src/database/models/Matches.ts
@@ -0,0 +1,38 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+import Teams from './Teams';
+
+class Matches extends Model {
+  declare id: number;
+  declare homeTeam: number;
+  declare homeTeamGoals: number;
+  declare awayTeam: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Matches.init({
+  id: {
+    primaryKey: true,
+    autoIncrement: true,
+    type: DataTypes.INTEGER,
+  },
+  homeTeam: { type: DataTypes.INTEGER },
+  homeTeamGoals: { type: DataTypes.INTEGER },
+  awayTeam: { type: DataTypes.INTEGER },
+  awayTeamGoals: { type: DataTypes.INTEGER },
+  inProgress: { type: DataTypes.BOOLEAN },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+Teams.hasMany(Matches, { foreignKey: 'homeTeam' });
+Teams.hasMany(Matches, { foreignKey: 'awayTeam' });
+
+Matches.belongsTo(Teams, { foreignKey: 'homeTeam' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeam' });
+
+export default Matches;
