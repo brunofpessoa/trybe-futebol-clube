33052ab92e68f0d44a023bcb6b42e89cef46421f
Bruno Pessoa <brunofernpessoa@gmail.com>
2022-11-28 13:03:41 -0300

    tests: query string da rota /matches

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -7,7 +7,7 @@ import App from '../app';
 import Matches from '../database/models/Matches';
 
 import { Response } from 'superagent';
-import { allMatchesMock } from './mocks/matches';
+import { allMatchesMock, finishedMatchesMock, inProgressMatchesMock } from './mocks/matches';
 
 chai.use(chaiHttp);
 
@@ -26,5 +26,21 @@ describe('Testes da rota /matches', () => {
     chaiHttpResponse = await chai.request(app).get('/matches')
     expect(chaiHttpResponse.status).to.be.equal(200);
     expect(chaiHttpResponse.body).to.be.deep.equal(allMatchesMock);
+  });
+
+  it('Deve retornar status 200 e a lista das partidas em andamento', async () => {
+    sinon.stub(Matches, "findAll").resolves(inProgressMatchesMock as unknown as Matches[]);
+
+    chaiHttpResponse = await chai.request(app).get('/matches?inProgress=true')
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.be.deep.equal(inProgressMatchesMock);
+  });
+
+  it('Deve retornar status 200 e a lista das partidas finalizadas', async () => {
+    sinon.stub(Matches, "findAll").resolves(finishedMatchesMock as unknown as Matches[]);
+
+    chaiHttpResponse = await chai.request(app).get('/matches?inProgress=false')
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.be.deep.equal(finishedMatchesMock);
   });
 });
--- app/backend/src/tests/mocks/matches.ts
+++ app/backend/src/tests/mocks/matches.ts
@@ -14,3 +14,37 @@ export const allMatchesMock = [
     }
   },
 ];
+
+export const inProgressMatchesMock = [
+  {
+    "id": 41,
+    "homeTeam": 16,
+    "homeTeamGoals": 2,
+    "awayTeam": 9,
+    "awayTeamGoals": 0,
+    "inProgress": true,
+    "teamHome": {
+      "teamName": "São Paulo"
+    },
+    "teamAway": {
+      "teamName": "Internacional"
+    }
+  },
+];
+
+export const finishedMatchesMock = [
+  {
+    "id": 1,
+    "homeTeam": 16,
+    "homeTeamGoals": 1,
+    "awayTeam": 8,
+    "awayTeamGoals": 1,
+    "inProgress": false,
+    "teamHome": {
+      "teamName": "São Paulo"
+    },
+    "teamAway": {
+      "teamName": "Grêmio"
+    }
+  },
+];
